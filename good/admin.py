236fc22e522b9f0ac47d2e4ff7ac83a7a593ad1d
Pavel <you@example.com>
2021-12-04 21:14:20 +0300

    Изменения N3

--- good/admin.py
+++ good/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Good)
+class GoodAdmin(admin.ModelAdmin):
+    list_display = ('id',  'cat_name', 'model', 'specifications', 'time_create', 'photo', 'is_published')
+    list_display_links = ('id', 'model')
+    search_fields = (['cat_name'])
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_create')
+    prepopulated_fields = {"slug": ("model",)}
+
+
+admin.site.register(Good, GoodAdmin)
