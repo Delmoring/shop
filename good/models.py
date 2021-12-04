f964eb8f8dac19447c25555ff9074552d21aa1aa
Pavel <you@example.com>
2021-12-04 20:56:40 +0300

    Изменения N2

--- good/models.py
+++ good/models.py
@@ -2,14 +2,15 @@ from django.db import models
 from django.urls import reverse
 
 class Good(models.Model):
-    cat_name = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
-    model = models.CharField(max_length=255)
-    specifications = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    cat_name = models.CharField(max_length=255, verbose_name="Название устройства" )
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL")
+    model = models.CharField(max_length=255, verbose_name="Модель")
+    specifications = models.TextField(blank=True, verbose_name="Технические характеристики")
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/", verbose_name="Фото")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+    time_update = models.DateTimeField(auto_now=True, verbose_name="Время изменения")
+    is_published = models.BooleanField(default=True, verbose_name="Публикация")
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категории")
 
     def __str__(self):
         return self.cat_name
