6f9fb4394dac8eef82c96c45ffe05574dedc13db
Pavel <you@example.com>
2022-01-15 22:24:57 +0300

    Все шаблоны наследуются от базового

--- good/views.py
+++ good/views.py
@@ -7,7 +7,7 @@ from good.models import Good
 good = Good.objects.all()
 def test_out(request):
 
-    return render(request, 'good/shop.html', {'good': good})
+    return render(request, 'good/index.html', {'good': good})
 
 def show_good(request, good_slug):
     show_device = get_object_or_404(Good, slug=good_slug)
